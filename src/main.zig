15d379722b1681d6806cd71f939529a58940e710
sergeypdev <sergeypoznyak@protonmail.com>
2021-05-05 13:25:44 +0300

    Add pf2 font support

--- src/main.zig
+++ src/main.zig
@@ -1,9 +1,12 @@
 const std = @import("std");
+const unicode = std.unicode;
 const sdl = @import("sdl.zig");
 const Audio = @import("audio.zig").Audio;
 const DumbFont16 = @import("dumbfont.zig").DumbFont16;
+const PF2Font = @import("pf2.zig").PF2Font;
 const bounce_bytes = @embedFile("../assets/bounce.wav");
 const main_font_bytes = @embedFile("../assets/04b_30.dumbfont16");
+const pf2_bytes = @embedFile("../assets/ka1.pf2");
 
 const Player = enum {
     left,
@@ -66,6 +69,7 @@ const Ball = struct {
 const GameAssets = struct {
     bounce_pcm: []const i16,
     main_font: *DumbFont16,
+    other_font: *PF2Font,
 };
 
 const GameState = struct {
@@ -119,6 +123,20 @@ const GameState = struct {
     }
 };
 
+const MainMenuButtons = enum {
+    Play,
+    Exit,
+};
+
+const MainMenuState = struct {
+    selectedItem: MainMenuButtons,
+};
+
+const GlobalGameState = union(enum) {
+    MainMenu: MainMenuState,
+    Game: GameState,
+};
+
 const BALL_SPEED = 5;
 const BALL_RADIUS = 20;
 const WIDTH = 800;
@@ -129,6 +147,8 @@ const PADDLE_WIDTH = 30;
 const MEM_SIZE = 1024 * 1024 * 128; // 128 megs
 
 pub fn main() anyerror!void {
+    var other_font = try PF2Font.fromConstMem(pf2_bytes);
+
     var game_mem = try std.heap.c_allocator.alloc(u8, MEM_SIZE);
 
     var allocator = std.heap.FixedBufferAllocator.init(game_mem);
@@ -167,6 +187,7 @@ pub fn main() anyerror!void {
     var assets = GameAssets{
         .bounce_pcm = @ptrCast([*c]i16, @alignCast(@alignOf(i16), bounce_pcm))[0 .. bounce_pcm_len / 2],
         .main_font = &main_font,
+        .other_font = &other_font,
     };
 
     var audio = Audio.init();
@@ -174,7 +195,7 @@ pub fn main() anyerror!void {
     try audio.open();
     audio.start();
 
-    var state = GameState.init();
+    var state = GlobalGameState{ .Game = GameState.init() };
 
     mainloop: while (true) {
         var event: sdl.SDL_Event = undefined;
@@ -191,168 +212,225 @@ pub fn main() anyerror!void {
     }
 }
 
-fn processInput(game_state: *GameState, event: sdl.SDL_KeyboardEvent) void {
-    switch (game_state.phase) {
-        .starting => {
-            game_state.phase = .playing;
-            game_state.ball.vx = BALL_SPEED;
-            game_state.ball.vy = BALL_SPEED;
-        },
-        .playing => {
-            switch (event.keysym.scancode) {
-                sdl.SDL_Scancode.SDL_SCANCODE_W => {
-                    game_state.left.up = event.state == sdl.SDL_PRESSED;
+fn processInput(global_game_state: *GlobalGameState, event: sdl.SDL_KeyboardEvent) void {
+    switch (global_game_state.*) {
+        .MainMenu => |*state| {},
+        .Game => |*game_state| {
+            switch (game_state.phase) {
+                .starting => {
+                    game_state.phase = .playing;
+                    game_state.ball.vx = BALL_SPEED;
+                    game_state.ball.vy = BALL_SPEED;
                 },
-                sdl.SDL_Scancode.SDL_SCANCODE_S => {
-                    game_state.left.down = event.state == sdl.SDL_PRESSED;
+                .playing => {
+                    switch (event.keysym.scancode) {
+                        sdl.SDL_Scancode.SDL_SCANCODE_W => {
+                            game_state.left.up = event.state == sdl.SDL_PRESSED;
+                        },
+                        sdl.SDL_Scancode.SDL_SCANCODE_S => {
+                            game_state.left.down = event.state == sdl.SDL_PRESSED;
+                        },
+                        sdl.SDL_Scancode.SDL_SCANCODE_UP => {
+                            game_state.right.up = event.state == sdl.SDL_PRESSED;
+                        },
+                        sdl.SDL_Scancode.SDL_SCANCODE_DOWN => {
+                            game_state.right.down = event.state == sdl.SDL_PRESSED;
+                        },
+                        else => {},
+                    }
                 },
-                sdl.SDL_Scancode.SDL_SCANCODE_UP => {
-                    game_state.right.up = event.state == sdl.SDL_PRESSED;
+                .finished => {
+                    game_state.restart();
                 },
-                sdl.SDL_Scancode.SDL_SCANCODE_DOWN => {
-                    game_state.right.down = event.state == sdl.SDL_PRESSED;
-                },
-                else => {},
             }
-        },
-        .finished => {
-            game_state.restart();
         },
     }
 }
 
-fn gameUpdateAndRender(assets: *GameAssets, state: *GameState, render: *sdl.Renderer, audio: *Audio) !void {
+fn gameUpdateAndRender(assets: *GameAssets, global_state: *GlobalGameState, render: *sdl.Renderer, audio: *Audio) !void {
     try render.setDrawColor(sdl.Color.black);
     try render.clear();
 
-    try render.setDrawColor(sdl.Color.white);
+    switch (global_state.*) {
+        .MainMenu => |*state| {},
+        .Game => |*state| {
+            try render.setDrawColor(sdl.Color.white);
 
-    // try render.fillRect(&.{ .x = 500, .y = 500, .w = 100, .h = 100 });
+            // try render.fillRect(&.{ .x = 500, .y = 500, .w = 100, .h = 100 });
 
-    switch (state.phase) {
-        .playing => {
-            if (state.left.up) {
-                state.left.y -= 10;
-            }
-            if (state.left.down) {
-                state.left.y += 10;
-            }
-            if (state.right.up) {
-                state.right.y -= 10;
-            }
-            if (state.right.down) {
-                state.right.y += 10;
+            switch (state.phase) {
+                .playing => {
+                    if (state.left.up) {
+                        state.left.y -= 10;
+                    }
+                    if (state.left.down) {
+                        state.left.y += 10;
+                    }
+                    if (state.right.up) {
+                        state.right.y -= 10;
+                    }
+                    if (state.right.down) {
+                        state.right.y += 10;
+                    }
+
+                    state.left.y = std.math.clamp(state.left.y, PADDLE_HEIGHT / 2, HEIGHT - (PADDLE_HEIGHT / 2));
+                    state.right.y = std.math.clamp(state.right.y, PADDLE_HEIGHT / 2, HEIGHT - (PADDLE_HEIGHT / 2));
+
+                    state.ball.x += state.ball.vx;
+                    state.ball.y += state.ball.vy;
+
+                    if (state.ball.bottom() >= HEIGHT) {
+                        state.ball.vy = -state.ball.vy;
+                    }
+                    if (state.ball.top() < 0) {
+                        state.ball.vy = -state.ball.vy;
+                    }
+
+                    if (state.ball.left() <= state.left.paddle_right() and state.ball.vx < 0) {
+                        var top = state.left.paddle_top() - state.ball.radius;
+                        var bottom = state.left.paddle_bottom() + state.ball.radius;
+                        if (state.ball.y >= top and state.ball.y <= bottom) {
+                            state.ball.vx = -state.ball.vx;
+                            audio.play(.{
+                                .offset = @as(u64, 0),
+                                .data = assets.bounce_pcm,
+                                .volume = 127,
+                            });
+                        }
+                    }
+                    if (state.ball.right() >= state.right.paddle_left() and state.ball.vx > 0) {
+                        var top = state.right.paddle_top() - state.ball.radius;
+                        var bottom = state.right.paddle_bottom() + state.ball.radius;
+                        if (state.ball.y >= top and state.ball.y <= bottom) {
+                            state.ball.vx = -state.ball.vx;
+                            audio.play(.{
+                                .offset = 1,
+                                .data = assets.bounce_pcm,
+                                .volume = 127,
+                            });
+                        }
+                    }
+
+                    if (state.ball.left() < 0) {
+                        state.phase = Phase{ .finished = .right };
+                        state.ball.radius = BALL_RADIUS * 2;
+                    } else if (state.ball.right() >= WIDTH) {
+                        state.phase = Phase{ .finished = .left };
+                        state.ball.radius = BALL_RADIUS * 2;
+                    }
+                },
+                else => {},
             }
 
-            state.left.y = std.math.clamp(state.left.y, PADDLE_HEIGHT / 2, HEIGHT - (PADDLE_HEIGHT / 2));
-            state.right.y = std.math.clamp(state.right.y, PADDLE_HEIGHT / 2, HEIGHT - (PADDLE_HEIGHT / 2));
+            // Left paddle
+            try render.fillRect(&.{
+                .x = 0,
+                .y = state.left.y - (PADDLE_HEIGHT / 2),
+                .w = PADDLE_WIDTH,
+                .h = PADDLE_HEIGHT,
+            });
 
-            state.ball.x += state.ball.vx;
-            state.ball.y += state.ball.vy;
+            // Left paddle
+            try render.fillRect(&.{
+                .x = WIDTH - PADDLE_WIDTH,
+                .y = state.right.y - (PADDLE_HEIGHT / 2),
+                .w = PADDLE_WIDTH,
+                .h = PADDLE_HEIGHT,
+            });
 
-            if (state.ball.bottom() >= HEIGHT) {
-                state.ball.vy = -state.ball.vy;
-            }
-            if (state.ball.top() < 0) {
-                state.ball.vy = -state.ball.vy;
-            }
+            try drawTextPf2(render, "Test, I'm a ball", state.ball.x, state.ball.y, assets.other_font, 2, 1);
 
-            if (state.ball.left() <= state.left.paddle_right() and state.ball.vx < 0) {
-                var top = state.left.paddle_top() - state.ball.radius;
-                var bottom = state.left.paddle_bottom() + state.ball.radius;
-                if (state.ball.y >= top and state.ball.y <= bottom) {
-                    state.ball.vx = -state.ball.vx;
-                    audio.play(.{
-                        .offset = @as(u64, 0),
-                        .data = assets.bounce_pcm,
-                        .volume = 127,
-                    });
-                }
-            }
-            if (state.ball.right() >= state.right.paddle_left() and state.ball.vx > 0) {
-                var top = state.right.paddle_top() - state.ball.radius;
-                var bottom = state.right.paddle_bottom() + state.ball.radius;
-                if (state.ball.y >= top and state.ball.y <= bottom) {
-                    state.ball.vx = -state.ball.vx;
-                    audio.play(.{
-                        .offset = 1,
-                        .data = assets.bounce_pcm,
-                        .volume = 127,
-                    });
-                }
-            }
+            // try render.fillRect(&.{
+            //     .x = state.ball.x - (@divTrunc(state.ball.radius, 2)),
+            //     .y = state.ball.y - (@divTrunc(state.ball.radius, 2)),
+            //     .w = state.ball.radius,
+            //     .h = state.ball.radius,
+            // });
 
-            if (state.ball.left() < 0) {
-                state.phase = Phase{ .finished = .right };
-                state.ball.radius = BALL_RADIUS * 2;
-            } else if (state.ball.right() >= WIDTH) {
-                state.phase = Phase{ .finished = .left };
-                state.ball.radius = BALL_RADIUS * 2;
+            const soundRatioX = WIDTH / @intToFloat(f32, audio.buffer_copy.len / 2);
+            const soundRatio = HEIGHT / @intToFloat(f32, std.math.maxInt(i16));
+            const graphSize = (soundRatio) * HEIGHT;
+
+            var i: usize = 0;
+            while (i < audio.buffer_copy.len / 2) : (i += 1) {
+                try render.fillRect(&.{
+                    .x = @floatToInt(c_int, @intToFloat(f32, i) * soundRatioX),
+                    .y = HEIGHT - @floatToInt(c_int, (graphSize)),
+                    .w = 1,
+                    .h = @floatToInt(c_int, @intToFloat(f32, audio.buffer_copy[i]) * soundRatio),
+                });
             }
         },
-        else => {},
     }
 
-    // Left paddle
-    try render.fillRect(&.{
-        .x = 0,
-        .y = state.left.y - (PADDLE_HEIGHT / 2),
-        .w = PADDLE_WIDTH,
-        .h = PADDLE_HEIGHT,
-    });
+    render.present();
+}
 
-    // Left paddle
-    try render.fillRect(&.{
-        .x = WIDTH - PADDLE_WIDTH,
-        .y = state.right.y - (PADDLE_HEIGHT / 2),
-        .w = PADDLE_WIDTH,
-        .h = PADDLE_HEIGHT,
-    });
+fn drawTextPf2(render: *sdl.Renderer, text: []const u8, screenX: i32, screenY: i32, font: *PF2Font, scale: i32, letterSpacing: i32) !void {
+    var offset: i32 = 0;
 
-    // Ball
-    var char: usize = '0';
+    var view = try unicode.Utf8View.init(text);
+    var iter = view.iterator();
 
-    while (char <= '9') : (char += 1) {
-        var offset = (char - '0') * 16 * 4;
-        var glyph = &assets.main_font.glyphs[char];
+    while (iter.nextCodepoint()) |codepoint| {
+        var glyph = font.getChar(codepoint) orelse continue;
 
         var y: usize = 0;
-        while (y < 16) : (y += 1) {
-            var row = glyph.pixels[y];
+        while (y < glyph.height) : (y += 1) {
             var x: usize = 0;
-            while (x < 16) : (x += 1) {
-                if ((row & @as(u16, 1) << @intCast(u4, x)) > 0) {
+            while (x < glyph.width) : (x += 1) {
+                var bitIndex = y * glyph.width + x;
+                var currentByte = glyph.pixels[bitIndex / 8];
+                var currentByteBitIndex = bitIndex % 8;
+                if (currentByte & (@as(u8, 0b10000000) >> @intCast(u3, currentByteBitIndex)) > 0) {
                     try render.fillRect(&.{
-                        .x = state.ball.x + @intCast(i32, x) * 4 + @intCast(i32, offset),
-                        .y = state.ball.y + @intCast(i32, y) * 4,
-                        .w = 4,
-                        .h = 4,
+                        .x = glyph.xOffset * scale + screenX + @intCast(i32, x) * scale + @intCast(i32, offset),
+                        .y = screenY + @intCast(i32, y) * scale - glyph.yOffset * scale - glyph.height * scale,
+                        .w = scale,
+                        .h = scale,
                     });
                 }
             }
         }
+
+        offset += glyph.deviceWidth * scale + letterSpacing;
     }
+}
 
-    // try render.fillRect(&.{
-    //     .x = state.ball.x - (@divTrunc(state.ball.radius, 2)),
-    //     .y = state.ball.y - (@divTrunc(state.ball.radius, 2)),
-    //     .w = state.ball.radius,
-    //     .h = state.ball.radius,
-    // });
+fn drawText(render: *sdl.Renderer, text: []const u8, x: i32, y: i32, font: *DumbFont16, scale: i32, letterSpacing: i32) !void {
+    var offset: i32 = 0;
+    for (text) |char, i| {
+        var glyph = &font.glyphs[char];
 
-    const soundRatioX = WIDTH / @intToFloat(f32, audio.buffer_copy.len / 2);
-    const soundRatio = HEIGHT / @intToFloat(f32, std.math.maxInt(i16));
-    const graphSize = (soundRatio) * HEIGHT;
+        var glyphY: usize = 0;
+        while (glyphY < 16) : (glyphY += 1) {
+            var row = glyph.pixels[glyphY];
+            var glyphX: usize = 0;
+            while (glyphX < 16) : (glyphX += 1) {
+                if ((row & @as(u16, 1) << @intCast(u4, glyphX)) > 0) {
+                    try render.fillRect(&.{
+                        .x = x + @intCast(i32, glyphX) * scale + @intCast(i32, offset),
+                        .y = y + @intCast(i32, glyphY) * scale,
+                        .w = scale,
+                        .h = scale,
+                    });
+                }
+            }
+        }
 
-    var i: usize = 0;
-    while (i < audio.buffer_copy.len / 2) : (i += 1) {
-        try render.fillRect(&.{
-            .x = @floatToInt(c_int, @intToFloat(f32, i) * soundRatioX),
-            .y = HEIGHT - @floatToInt(c_int, (graphSize)),
-            .w = 1,
-            .h = @floatToInt(c_int, @intToFloat(f32, audio.buffer_copy[i]) * soundRatio),
-        });
+        switch (char) {
+            '1', 'i', 'I' => {
+                offset += 10 * scale + letterSpacing;
+            },
+            ' ' => {
+                offset += 4 * scale;
+            },
+            '\'' => {
+                offset += 8 * scale;
+            },
+            else => {
+                offset += 16 * scale + letterSpacing;
+            },
+        }
     }
-
-    render.present();
 }
---
+++ src/pf2.zig
@@ -0,0 +1,196 @@
+const std = @import("std");
+const builtin = std.builtin;
+
+// Format reference: http://grub.gibibit.com/New_font_format
+
+pub const PF2Font = struct {
+    name: []const u8,
+    family: []const u8,
+    weight: Weight,
+    slant: Slant,
+    pointSize: u16,
+    maxWidth: u16,
+    maxHeight: u16,
+    ascent: u16,
+    descent: u16,
+    chix: []const u8,
+    dataOffset: u32,
+    data: []const u8,
+
+    pub const Weight = enum {
+        bold,
+        normal,
+    };
+
+    pub const Slant = enum {
+        italic,
+        normal,
+    };
+
+    pub const signature = "PFF2";
+
+    pub const CharIndexEntry = packed struct {
+        codepoint: u32,
+        flags: u8,
+        offset: u32,
+    };
+
+    pub const RawDataEntry = packed struct {
+        width: u16,
+        height: u16,
+        xOffset: i16,
+        yOffset: i16,
+        deviceWidth: i16,
+    };
+
+    pub const CharEntry = struct {
+        width: u16,
+        height: u16,
+        xOffset: i16,
+        yOffset: i16,
+        deviceWidth: i16,
+        pixels: []const u8,
+    };
+
+    const Section = enum {
+        file,
+        name,
+        family,
+        weight,
+        slant,
+        pointSize,
+        maxWidth,
+        maxHeight,
+        ascent,
+        descent,
+        chix,
+        data,
+    };
+
+    fn parseSection(str: []const u8) !Section {
+        std.debug.assert(str.len == 4);
+
+        if (std.mem.eql(u8, str, "FILE")) {
+            return .file;
+        } else if (std.mem.eql(u8, str, "NAME")) {
+            return .name;
+        } else if (std.mem.eql(u8, str, "FAMI")) {
+            return .family;
+        } else if (std.mem.eql(u8, str, "WEIG")) {
+            return .weight;
+        } else if (std.mem.eql(u8, str, "SLAN")) {
+            return .slant;
+        } else if (std.mem.eql(u8, str, "PTSZ")) {
+            return .pointSize;
+        } else if (std.mem.eql(u8, str, "MAXW")) {
+            return .maxWidth;
+        } else if (std.mem.eql(u8, str, "MAXH")) {
+            return .maxHeight;
+        } else if (std.mem.eql(u8, str, "ASCE")) {
+            return .ascent;
+        } else if (std.mem.eql(u8, str, "DESC")) {
+            return .descent;
+        } else if (std.mem.eql(u8, str, "CHIX")) {
+            return .chix;
+        } else if (std.mem.eql(u8, str, "DATA")) {
+            return .data;
+        }
+
+        return error.InvalidSection;
+    }
+
+    pub fn fromConstMem(mem: []const u8) !PF2Font {
+        var offset: usize = 0;
+
+        var font: PF2Font = undefined;
+
+        while (offset < mem.len) {
+            var sectionType = try parseSection(mem[offset .. offset + 4]);
+            offset += 4;
+            var sectionLen = std.mem.readIntSlice(u32, mem[offset..], .Big);
+            offset += 4;
+
+            var section: []const u8 = undefined;
+
+            var sectionStartOffset = offset;
+
+            if (sectionType == .data) {
+                section = mem[offset..mem.len];
+                offset = mem.len;
+            } else {
+                section = mem[offset .. offset + sectionLen];
+                offset += sectionLen;
+            }
+
+            switch (sectionType) {
+                .file => {
+                    if (!std.mem.eql(u8, section, signature)) {
+                        return error.WrongSignature;
+                    }
+                },
+                .name => font.name = section,
+                .family => font.family = section,
+                .weight => switch (section[0]) {
+                    'b' => font.weight = .bold,
+                    'n' => font.weight = .normal,
+                    else => return error.WrongWeight,
+                },
+                .slant => switch (section[0]) {
+                    'i' => font.slant = .italic,
+                    'n' => font.slant = .normal,
+                    else => return error.WrongSlant,
+                },
+                .pointSize => font.pointSize = std.mem.readIntSlice(u16, section, .Big),
+                .maxWidth => font.maxWidth = std.mem.readIntSlice(u16, section, .Big),
+                .maxHeight => font.maxHeight = std.mem.readIntSlice(u16, section, .Big),
+                .ascent => font.ascent = std.mem.readIntSlice(u16, section, .Big),
+                .descent => font.descent = std.mem.readIntSlice(u16, section, .Big),
+                .chix => font.chix = section,
+                .data => {
+                    font.data = section;
+                    font.dataOffset = @intCast(u32, sectionStartOffset);
+                },
+            }
+        }
+
+        return font;
+    }
+
+    pub fn findCharIndex(self: *const PF2Font, needle: u32) ?CharIndexEntry {
+        var i: usize = 0;
+        while (i < self.chix.len) : (i += 9) {
+            var codepoint = std.mem.readIntSlice(u32, self.chix[i..], .Big);
+            var result: CharIndexEntry = undefined;
+            result.codepoint = codepoint;
+            result.flags = self.chix[i + 4];
+            result.offset = std.mem.readIntSlice(u32, self.chix[i + 5 ..], .Big) - self.dataOffset;
+
+            if (needle == codepoint) {
+                return result;
+            }
+        }
+
+        return null;
+    }
+
+    pub fn getChar(self: *const PF2Font, needle: u32) ?CharEntry {
+        var chix = self.findCharIndex(needle) orelse return null;
+
+        std.debug.warn("Char {} offset {}\n", .{ needle, chix.offset });
+        var data = self.data[chix.offset..];
+
+        var width = std.mem.readIntSlice(u16, data, .Big);
+        var height = std.mem.readIntSlice(u16, data[2..], .Big);
+
+        var entry = CharEntry{
+            .width = width,
+            .height = height,
+            .xOffset = std.mem.readIntSlice(i16, data[4..], .Big),
+            .yOffset = std.mem.readIntSlice(i16, data[6..], .Big),
+            .deviceWidth = std.mem.readIntSlice(i16, data[8..], .Big),
+            .pixels = @ptrCast([*]const u8, self.data[chix.offset + @sizeOf(RawDataEntry) ..])[0 .. (width * height + 7) / 8],
+        };
+
+        return entry;
+    }
+};
